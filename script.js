cf8a9b92fe473aeacbf4acbd7eda963bc703456d
Maksim <maxgraph23@gmail.com>
2023-08-13 22:50:31 +0300

    initial commit

---
+++ script.js
@@ -0,0 +1,67 @@
+window.addEventListener('DOMContentLoaded', () => {
+
+  const resizableSwiper = (breakpoint, swiperClass, swiperSettings, callback) => {
+    let swiper;
+
+    breakpoint = window.matchMedia(breakpoint);
+
+    const enableSwiper = function(className, settings) {
+      swiper = new Swiper(className, settings);
+
+      if (callback) {
+        callback(swiper);
+      }
+    }
+
+    const checker = function() {
+      if (breakpoint.matches) {
+        return enableSwiper(swiperClass, swiperSettings);
+      } else {
+        if (swiper !== undefined) swiper.destroy(true, true);
+        return;
+      }
+    };
+
+    breakpoint.addEventListener('change', checker);
+    checker();
+  }
+
+  const someFunc = (instance) => {
+    if (instance) {
+      instance.on('slideChange', function (e) {
+        console.log('*** mySwiper.activeIndex', instance.activeIndex);
+      });
+    }
+  };
+
+  resizableSwiper(
+    '(max-width: 1280px)',
+    '.slider-1',
+    {
+      loop: true,
+      spaceBetween: 32,
+      slidesPerView: 1,
+      pagination: {
+        el: '.swiper-pagination',
+        clickable: true,
+      },
+    },
+    someFunc
+  );
+
+  resizableSwiper(
+    '(max-width: 1500px)',
+    '.slider-2',
+    {
+      loop: true,
+      spaceBetween: 10,
+      slidesPerView: 3,
+      freeMode: true,
+      breakpoints: {
+        1200: {
+          spaceBetween: 20,
+        }
+      }
+    }
+  );
+});
